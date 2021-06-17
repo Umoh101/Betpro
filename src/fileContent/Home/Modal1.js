c80eeb81d5d8a2bfe4d910a8e6c8b1d31db9b908
Umoh101 <85814084+Umoh101@users.noreply.github.com>
2021-06-17 21:20:14 +0100

    Update Modal1.js

--- src/fileContent/Home/Modal1.js
+++ src/fileContent/Home/Modal1.js
@@ -1,28 +1,12 @@
 import React from "react";
 import {useHistory} from "react-router-dom";
 import {Button} from "antd";
-import {makeStyles} from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 
-const useStyles = makeStyles(theme => ({
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
-
 export default function Modal1() {
   const hist = useHistory();
-  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
